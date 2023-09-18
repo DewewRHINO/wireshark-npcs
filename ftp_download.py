a6b77feeb794cc2265d34e0f053394d2cdc5eeae
Jacob Jayme <97200568+DewewRHINO@users.noreply.github.com>
2023-09-17 20:01:25 -0700

    Updated FTP to recursively download.

--- ftp_download.py
+++ ftp_download.py
@@ -1,25 +1,55 @@
 from ftplib import FTP
+import os
 import sys
 
-def ftp_download(host, username, password, remote_file, local_file):
+def ftp_download_dir(ftp, remote_dir, local_dir):
     try:
-        ftp = FTP(host)
-        ftp.login(username, password)
-        ftp.retrbinary("RETR " + remote_file, open(local_file, "wb").write)
-        ftp.quit()
-        print(f"Downloaded {remote_file} to {local_file}")
+        # Create local directory if it doesn't exist
+        if not os.path.exists(local_dir):
+            os.makedirs(local_dir)
+
+        # Change remote directory
+        ftp.cwd(remote_dir)
+
+        # List files and directories in the current remote directory
+        remote_items = ftp.nlst()
+
+        for item in remote_items:
+            local_item = os.path.join(local_dir, item)
+
+            if ftp.nlst(item) == []:
+                # If it's a file, download it
+                with open(local_item, "wb") as f:
+                    ftp.retrbinary("RETR " + item, f.write)
+                print(f"Downloaded {item} to {local_item}")
+            else:
+                # If it's a directory, recurse into it
+                ftp_download_dir(ftp, item, local_item)
+
+        # Go back up one level in the remote directory hierarchy
+        ftp.cwd("..")
+
     except Exception as e:
         print(f"FTP Download Error: {str(e)}")
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) != 6:
-        print("Usage: python ftp_download.py FTP_HOST FTP_USER FTP_PASS FTP_REMOTE_FILE FTP_LOCAL_FILE")
+        print("Usage: python ftp_download_dir.py FTP_HOST FTP_USER FTP_PASS FTP_REMOTE_DIR FTP_LOCAL_DIR")
         sys.exit(1)
 
     ftp_host = sys.argv[1]
     ftp_user = sys.argv[2]
     ftp_pass = sys.argv[3]
-    ftp_remote_file = sys.argv[4]
-    ftp_local_file = sys.argv[5]
+    ftp_remote_dir = sys.argv[4]
+    ftp_local_dir = sys.argv[5]
 
-    ftp_download(ftp_host, ftp_user, ftp_pass, ftp_remote_file, ftp_local_file)
+    try:
+        ftp = FTP(ftp_host)
+        ftp.login(ftp_user, ftp_pass)
+        ftp_download_dir(ftp, ftp_remote_dir, ftp_local_dir)
+        ftp.quit()
+    except Exception as e:
+        print(f"FTP Connection Error: {str(e)}")
+
+if __name__ == "__main__":
+    main()
